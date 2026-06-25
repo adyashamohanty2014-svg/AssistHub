356be19f2c919fff60769ea6997f2cc748271ded
adyashamohanty2014-svg <adyashamohanty2014@gmail.com>
2026-06-25 21:05:58 +0530

    Updated AssistHub category and logout

--- products/models.py
+++ products/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    image = models.ImageField(
+        upload_to='categories/',
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.name
--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
  path('',views.home, name='home'),
  path('login/', views.user_login, name='login'),
@@ -7,4 +8,5 @@ urlpatterns = [
  path('devices/', views.device_list, name='device_list'),
  path('device/<int:id>/', views.device_detail, name='device_detail'),
  path('category/<int:category_id>/',views.category_devices,name='category_devices'),
+ path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
