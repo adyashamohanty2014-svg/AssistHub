19869d1d28fab5ff566e99c254e8babeeb9dd8f3
adyashamohanty2014-svg <adyashamohanty2014@gmail.com>
2026-08-06 21:58:19 +0530

    add collected static files

--- AssistHub/urls.py
+++ AssistHub/urls.py
@@ -53,8 +53,8 @@ urlpatterns = [
     ),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
+
+urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
