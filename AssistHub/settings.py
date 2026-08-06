875dd733651cbb165490926e1084779c5e63bfd6
adyashamohanty2014-svg <adyashamohanty2014@gmail.com>
2026-08-06 19:53:39 +0530

    Added gunicorn

--- AssistHub/settings.py
+++ AssistHub/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = 'django-insecure-n(e3fih(f9cpk#7xekv9w^48-p4*=sau+@$1k4i8n9duxr)^i8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com",
+]
 
 # Application definition
 
@@ -120,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
 BASE_DIR / 'static',
 ]
